07f198fa74f0001c555007e71c7eff722d74cd89
Nick <nick.truong.gos@gmail.com>
2024-02-28 17:27:08 +0700

    refactor: env variables

--- src/config/envs/default.ts
+++ src/config/envs/default.ts
@@ -1,6 +1,11 @@
 import { Time } from '@/utils/constants';
 
 export const config = {
+  admin: {
+    name: process.env.ADMIN_NAME || 'admin',
+    email: process.env.ADMIN_EMAIL || 'admin@example.com',
+    password: process.env.ADMIN_PASSWORD || 'admin123',
+  },
   db: {
     type: process.env.DB_TYPE || 'postgres',
     host: process.env.DB_HOST || 'localhost',
