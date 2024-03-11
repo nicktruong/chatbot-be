ec73bcb9e40c6e26df5bdeccfc9d327f320747a2
Nick <nick.truong.gos@gmail.com>
2024-03-11 14:31:08 +0700

    feat: support fields for capture information cards

--- src/database/seeds/card-type/card-type-seed.service.ts
+++ src/database/seeds/card-type/card-type-seed.service.ts
@@ -7,28 +7,54 @@ import { CardTypeEnum, GroupTypeEnum } from '@/api/card-type/card-type.enum';
 
 @Injectable()
 export class CardTypeSeedService {
+  cards = [
+    {
+      desc: '',
+      name: 'Expression',
+      type: CardTypeEnum.EXPRESSION,
+      groupType: GroupTypeEnum.TRANSITION,
+    },
+    {
+      desc: '',
+      name: 'Number',
+      type: CardTypeEnum.NUMBER,
+      groupType: GroupTypeEnum.CAPTURE_INFO,
+    },
+  ];
+
   constructor(
     @InjectRepository(CardType) private repository: CardTypeRepository,
   ) {}
 
-  async seedExpressionCardType() {
+  async seedCardType({
+    desc,
+    name,
+    type,
+    groupType,
+  }: {
+    desc: string;
+    name: string;
+    type: CardTypeEnum;
+    groupType: GroupTypeEnum;
+  }) {
     const countCardType = await this.repository.count({
-      where: { type: CardTypeEnum.EXPRESSION },
+      where: { type },
     });
 
     if (!countCardType) {
+      // Don't use .create([]) here as we may modify cards
       await this.repository.save(
         this.repository.create({
-          desc: '',
-          groupType: GroupTypeEnum.TRANSITION,
-          type: CardTypeEnum.EXPRESSION,
-          name: 'Expression',
+          desc,
+          name,
+          type,
+          groupType,
         }),
       );
     }
   }
 
   async run() {
-    await this.seedExpressionCardType();
+    await Promise.all(this.cards.map((card) => this.seedCardType(card)));
   }
 }
