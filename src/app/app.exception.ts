27737e7d9dd949e7b4886c878c5007f261741522
Nick <nick.truong.gos@gmail.com>
2024-03-20 09:27:12 +0700

    feat: transform response to dto interceptor

--- src/app/app.exception.ts
+++ src/app/app.exception.ts
@@ -1,14 +1,10 @@
-import { Reflector } from '@nestjs/core';
-import type { INestApplication } from '@nestjs/common';
-import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { ValidationPipe, type INestApplication } from '@nestjs/common';
 
 import { getPipeOptions } from '@/config';
 import { AdvancedExceptionFilter, ValidatorExceptionFilter } from '@/filters';
 
 export const loadErrorHandling = (app: INestApplication): void => {
   app.useGlobalPipes(new ValidationPipe(getPipeOptions()));
-
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.useGlobalFilters(
     new AdvancedExceptionFilter(),
