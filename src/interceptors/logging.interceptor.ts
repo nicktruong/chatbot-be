6bf9f3760ff8f11a4e830e109d41292935bb1fd5
Nick <nick.truong.gos@gmail.com>
2024-03-25 13:35:09 +0700

    refactor: change console.log to logger from nestjs in logging interceptor

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -1,4 +1,5 @@
 import {
+  Logger,
   Injectable,
   CallHandler,
   NestInterceptor,
@@ -9,12 +10,16 @@ import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  private logger = new Logger('Interceptor');
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before reaching the handler...');
+    this.logger.log('Before reaching the handler...');
 
     const now = Date.now();
     return next
       .handle()
-      .pipe(tap(() => console.log(`Response Lag... ${Date.now() - now}ms`)));
+      .pipe(
+        tap(() => this.logger.log(`Response Lag... ${Date.now() - now}ms`)),
+      );
   }
 }
